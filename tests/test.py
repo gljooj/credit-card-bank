a2a3c58eb1291e0e625158329ece6538e8d95fc1
Gabriel Jesus <gabrielthe13@hotmail.com>
2024-01-11 23:43:15 -0300

    Fixing the tests

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,8 @@
+import os
 import sys
 import unittest
-import os
-from past.builtins import execfile
+import subprocess
+
 from tests.tmp_operations import test_operations
 
 here = os.path.dirname(os.path.realpath(__file__))
@@ -19,10 +20,16 @@ class Tests(unittest.TestCase):
     def test_authorize(self):
         n = 1
         for test in test_operations:
-            print(n)
+            print(f"Operation test: {n}")
             n = n + 1
             self.create_temp_file(test)
             in_file = open(here+'/tmp_operations/temp_operation_file', 'r')
             sys.stdin = in_file
-            execfile(here+"/../authorizator.py")
-            in_file.close()
+            command = ['python3', here+"/../authorizator.py"]
+            try:
+
+                resultado = subprocess.run(command, stdin=in_file, capture_output=True, text=True)
+                print(resultado.stdout)
+                assert True
+            except Exception as e:
+                raise Exception(f"Fail to run test {n} error: {e}")
